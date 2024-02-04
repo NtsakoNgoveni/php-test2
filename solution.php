91741a44374af704054c2c6814464104268ffea4
Ntsako Ngoveni <naction923@gmail.com>
2024-02-05 01:01:10 +0200

    Task 3
    Refactored product class to allow integration with spreasheet
    
    Added the fetch_product_list method to get products from spreadsheet

--- solution.php
+++ solution.php
@@ -1,19 +1,50 @@
 <?php
 
+require __DIR__ ."/vendor/autoload.php";
 class Product {
     private $products;
+    private $spreadsheetId;
 
-    public function __construct($products) {
-        $this->products = $products;
+    public function __construct($spreadsheetId) {
+        $this->spreadsheetId = $spreadsheetId;
+        $this->products = $this->fetch_product_list($spreadsheetId);
     }
 
-    public function setProducts($newProducts) {
-        $this->products = $newProducts;
+    public function fetch_product_list($spreadsheetId) {
+        $client = new Google_Client();
+        $client->setApplicationName("sheets_integration");
+        $client->addScope(Google_Service_Sheets::SPREADSHEETS);
+        $client->setAuthConfig(__DIR__ . "/credentials.json");
+        $service = new Google_Service_Sheets($client);
+    
+    
+        $sheetsMetadata = $service->spreadsheets->get($spreadsheetId)->getSheets();
+        $products = [];
+        foreach ($sheetsMetadata as $sheetMetadata) {
+            $sheetName = $sheetMetadata->properties->title;
+            $range = $sheetName;
+            $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+            $values = $response->getValues();
+    
+            if (empty($values)) {
+                echo "No data found in sheet '$sheetName'.\n";
+            } else {
+                $keys = array_shift($values);
+    
+                foreach ($values as $row) {
+                    array_push($products, array_combine($keys,$row));
+                }
+            }
+        }
+         return $products;
+    }
+    public function setProducts($spreadsheetId) {
+        $this->products = $this->fetch_product_list($spreadsheetId);
     }
     public function get_product($productId){
         $productDetails = [];
         foreach ($this->products as $product) {
-            if($product['id'] == $productId) {
+            if($product['id'] === $productId) {
                 $productDetails = $product;
             }
         }
@@ -21,12 +52,4 @@ class Product {
     }
 }
 
-$products = [
-    ['id' => 101, 'name' => 'Product 1', 'price' => 99.99],
-    ['id' => 102, 'name' => 'Product 2', 'price' => 75.00],
-    ['id' => 103, 'name' => 'Product 3', 'price' => 120.00],
-];
-
-$product = new Product($products);
-print_r($product->get_product(101));
 ?>
